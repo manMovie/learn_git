4a53d93c299ee51cc111637f706c4431e6fbb646
manMovie <manmovie162@gmail.com>
2025-04-06 10:09:48 +0530

    learn first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello Git");
+// git status -s for short of status
+// git status for all status
+// git add <file>/. to add files
+
+// git config --global user.email "email@email.com"
+// git config --global user.name "name"
